3f3fa4ab2d5831af17408dec4afab7a3c994a331
Aisiti03 <143867535+Aisiti03@users.noreply.github.com>
2023-10-02 14:32:27 +0700

    Add files via upload

---
+++ profile_arrow_function.js
@@ -0,0 +1,7 @@
+const login = (namaDepan, namaBelakang, email) => {
+  console.log("Login Screen")
+  console.log("Hello" + namaDepan + namaBelakang)
+  console.log("Email :" + email)
+}
+
+profile("Aisiti","mutmainah","aisitimutmainah60@gmail.com")
---
+++ profile_reg_function.js
@@ -0,0 +1,6 @@
+function profile (namaDepan, namaBelakang,email){
+  console.log("Nama :" + namaDepan + namaBelakang + email)
+  console.log("Email :" + email)
+}
+
+profile("Aisiti","mutmainah","aisitimutmainah60@gmail.com")
